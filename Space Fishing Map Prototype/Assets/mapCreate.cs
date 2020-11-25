547f9eff0f094fdcb9e23ee82e28d0e56f63465f
jwrsandilands <55706784+jwrsandilands@users.noreply.github.com>
2020-11-25 14:16:59 +0000

    Map Prototype Created
    I have worked on a Space Map Generation Prototype, where planets spawn randomly within a given area of the map.

---
+++ Space Fishing Map Prototype/Assets/mapCreate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mapCreate : MonoBehaviour
+{
+    public GameObject Planet;
+
+    public float xMin, xMax, yMin, yMax;
+    public int maxPlanets;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int count = 1;
+        while(count <= maxPlanets)
+        {
+            Vector3 pos = new Vector3(Random.Range(xMin, xMax), Random.Range(yMin, yMax), 0);
+            Quaternion rotate = new Quaternion(0, 0, 0, 0);
+            
+            Instantiate(Planet, pos, rotate);
+            count++;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

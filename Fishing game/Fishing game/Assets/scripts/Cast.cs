61dbdf965b2c45bfab84cb829a522e2d87ea2f6c
jamount <55700813+jamount@users.noreply.github.com>
2020-12-18 18:48:01 +0000

    Added aiming of the cast
    can now aim the cast + deleted redundant files

---
+++ Fishing game/Fishing game/Assets/scripts/Cast.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cast : MonoBehaviour
+{
+
+    public Animator castAnimation;
+
+    private bool casting;
+    private float atHookTime = 0.9f;
+    private float atHook;
+
+
+    public Transform castPoint;
+    public Transform CrossHair;
+    public GameObject hookprefab;
+    private float angle;
+    private Quaternion q;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            casting = true;
+            CastHookAnim();
+            
+        }
+
+        if(casting && Time.time > atHook)
+        {
+            
+
+            CastHook();
+           
+            
+
+        }
+    }
+
+    void CastHookAnim()
+    {
+        castAnimation.Play("Cast");
+        atHook = Time.time + atHookTime;
+        casting = true;
+        
+        
+    }
+
+    void CastHook()
+    {
+        casting = false;
+        Vector3 dir = CrossHair.position - castPoint.position;
+        angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        Quaternion q = Quaternion.Euler(0f, 0f, angle);
+
+        Debug.Log("q = " + q);
+        Instantiate(hookprefab, castPoint.position, q);
+    }
+}

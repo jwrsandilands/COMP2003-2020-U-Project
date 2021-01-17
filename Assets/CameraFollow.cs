f8db0d718b1fe38a5a78eb8db7501383945d3fab
jamount <55700813+jamount@users.noreply.github.com>
2021-01-17 18:34:44 +0000

    smoothened the camera follow

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    Transform target;
+    Transform camTrans;
+
+    public float smoothFloat = 0.125f;
+    public Vector3 offset;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camTrans = Camera.main.GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        //finds target to follow
+        target = GameObject.FindGameObjectWithTag("Hook").transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //sets camera to target
+        transform.position = target.position + offset;
+    }
+}

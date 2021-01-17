668d23c08a0c168f764c44c85a0d57a2529ece44
jamount <55700813+jamount@users.noreply.github.com>
2021-01-17 20:45:14 +0000

    added background
    attempted to get hook to slow in the planet area but with no success

--- Assets/scripts/HookLaunch.cs
+++ Assets/scripts/HookLaunch.cs
@@ -1,19 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HookLaunch : MonoBehaviour
 {
 
-    public float speed = 50f;
+    public float speed;
     public Rigidbody2D rb;
+    
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        speed = GameObject.Find("VelocitySlider").GetComponent<Slider>().value;
+        rb.gravityScale = 1;
         rb.velocity = transform.right * speed;
-        GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraFollowHook>().SwitchTarget(gameObject.transform);
     }
 
-    
+    private void Update()
+    {
+       
+        
+    }
+
+    private void slow()
+    {
+        
+       
+    }
+
+
 }

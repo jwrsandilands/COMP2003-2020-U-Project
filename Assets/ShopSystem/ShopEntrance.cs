4a94ae8fa46827a0875b7be570b1778df301c4f7
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2021-04-20 21:57:37 +0100

    Feat: Shop Entrance script
    Added the shop entrance script into the project.

---
+++ Assets/ShopSystem/ShopEntrance.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopEntrance : MonoBehaviour
+{
+    public GameObject shopUI;
+    public GameObject scoreUI;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Character"))
+        {
+            shopUI.SetActive(true); 
+            scoreUI.SetActive(true);
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+
+        }
+    }
+}

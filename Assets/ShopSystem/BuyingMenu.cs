98582727e27eacd24319a52cb81282c13805267a
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2021-04-20 22:00:06 +0100

    feat: added Buying Menu script
    Added Buying Menu script to the project.

---
+++ Assets/ShopSystem/BuyingMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuyingMenu : MonoBehaviour
+{
+    public Image buyingPanel;
+
+    private void OnBecameVisible()
+    {
+        buyingPanel.color = Color.blue;
+    }
+
+    public void ChangeColourToRed() 
+    {
+        buyingPanel.color = Color.red;
+    }
+
+    public void ChangeColourToGreen() 
+    {
+        buyingPanel.color = Color.green;
+    }
+
+    public void ChangeColourToBlue() 
+    {
+        buyingPanel.color = Color.blue;
+    }
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}

ed70efba4f3dfc9010c46c6ba7d4b0a2976d03e4
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2021-05-10 19:38:55 +0100

    Updated BuyingMenu.cs
    Added in code for the hooks, rods and bait so that they can be displayed in the transaction menu.

--- Assets/ShopSystem/BuyingMenu.cs
+++ Assets/ShopSystem/BuyingMenu.cs
@@ -5,39 +5,92 @@ using UnityEngine.UI;
 
 public class BuyingMenu : MonoBehaviour
 {
-    public Image buyingPanel;
+    public Hook[] hooks;
 
-    private void OnBecameVisible()
+    public RodItem[] rods;
+
+    public BaitItem[] baits;
+
+
+    public Image[] images;
+
+    public Text[] textPrices;
+
+    public TransactionMenu transactionMenu;
+
+    public GameObject buyingMenuPanel;
+
+    public static bool isHook;
+    public static bool isRod;
+    public static bool isBait;
+
+
+    private void OnEnable()
     {
-        buyingPanel.color = Color.blue;
+        DisplayHookProducts();
     }
 
-    public void ChangeColourToRed() 
+    public void DisplayRodProducts()
     {
-        buyingPanel.color = Color.red;
+        isRod = true;
+        isHook = false;
+        isBait = false;
+
+        for(int i = 0; i < rods.Length; i++)
+        {
+            images[i].sprite = rods[i].getImage();
+            textPrices[i].text = rods[i].getPrice().ToString();
+        }
     }
 
-    public void ChangeColourToGreen() 
+    public void DisplayBaitProducts()
     {
-        buyingPanel.color = Color.green;
+        isRod = false;
+        isHook = false;
+        isBait = true;
+
+        for (int i = 0; i < baits.Length; i++)
+        {
+            images[i].sprite = baits[i].getImage();
+            textPrices[i].text = baits[i].getPrice().ToString();
+        }
     }
 
-    public void ChangeColourToBlue() 
+
+    public void DisplayHookProducts()
     {
-        buyingPanel.color = Color.blue;
+        isHook = true;
+        isRod = false;
+        isBait = false;
+
+        for (int i = 0; i < hooks.Length; i++)
+        {
+            images[i].sprite = hooks[i].getImage();
+            textPrices[i].text = hooks[i].getPrice().ToString();
+        }
     }
 
 
-
-
-
-    // Start is called before the first frame update
-    void Start()
-    { 
-    }
-
-    // Update is called once per frame
-    void Update()
+    public void OpenTransactionMenu(int index)
     {
+        if (isHook)
+        {
+            buyingMenuPanel.SetActive(false);
+            transactionMenu.menuPanel.SetActive(true);
+            transactionMenu.DisplayHookData(hooks, index);
+        }
+        else if(isRod)
+        {
+            buyingMenuPanel.SetActive(false);
+            transactionMenu.menuPanel.SetActive(true);
+            transactionMenu.DisplayRodData(rods, index);
+        }
+        else if(isBait)
+        {
+            buyingMenuPanel.SetActive(false);
+            transactionMenu.menuPanel.SetActive(true);
+            transactionMenu.DisplayBaitData(baits, index);
+        }
+        
     }
 }

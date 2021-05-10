ef60d6ac6a73a91b827c3c395f6a451ee3e1c10d
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2021-05-10 19:36:30 +0100

    Updated FishManager
    Added comments in the code and improved the code to a fix a bug.

--- Assets/ShopSystem/FishManager.cs
+++ Assets/ShopSystem/FishManager.cs
@@ -5,33 +5,38 @@ using UnityEngine.UI;
 
 public class FishManager : MonoBehaviour
 {
-    private int value;
+    public int value = -1; //holds the current amount of fish
 
-    private FishItem fish;
-    public Text valueText;
+    private FishItem fish; //this is for the fish object
+    public Text valueText; //this is for when the fish gets updated and for showing the amount onscreen
 
-    public FishInventory inventory;
-
+    public FishInventory inventory; //a reference for our fish inventory 
 
     private void Start()
     {
-        fish = FindObjectOfType<FishItem>();
+        fish = FindObjectOfType<FishItem>(); //gets the fish object
     }
 
     public void AddValue()
     {
-        value++;
-        Debug.Log(value);
-        fish.SetValue(value);
-        valueText.text = fish.GetValue().ToString();
-        inventory.AddToArray(fish);
+        
+        inventory.AddToArray(fish); //adds the fish to the array (calls the function)
+        if (!inventory.isFull)
+        {
+            value++; //increments the value
+            Debug.Log(value); //displays the value to the console
+            fish.SetValue(value); //sets the fish value
+            valueText.text = fish.GetValue().ToString(); //shows the fish value onscreen
+        }
+         
+        
     }
 
     public void SubtractValue(int subValue)
     {
-        value = value - subValue;
-        Debug.Log(value);
-        fish.SetValue(value);
-        valueText.text = fish.GetValue().ToString();
+        value = value - subValue; //subtracts the value by the subvalue parameter
+        Debug.Log(value); //displays the value to the console
+        fish.SetValue(value); //sets the fish value
+        valueText.text = fish.GetValue().ToString();  //shows the fish value onscreen
     }
 }

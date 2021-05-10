9c016a87553d668c32ebb41d42375cc8e285103f
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2021-05-10 19:37:29 +0100

    Updated EnableMenu.cs
    Added comments to the code

--- Assets/ShopSystem/EnableMenu.cs
+++ Assets/ShopSystem/EnableMenu.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class EnableMenu : MonoBehaviour
 {
-    public GameObject buyingMenuPanel;
-    public GameObject sellingMenuPanel;
+    public GameObject buyingMenuPanel; //this is for the buying menu panel
+    public GameObject sellingMenuPanel; //this is for the selling menu panel
 
     public void EnableBuyingMenu() 
     {
+        //enables the buying menu and disables the selling menu
         sellingMenuPanel.SetActive(false); 
         buyingMenuPanel.SetActive(true);
     }
 
     public void EnableSellingMenu() 
     {
+        //enable the selling menu and disables the buying menu
         buyingMenuPanel.SetActive(false);
         sellingMenuPanel.SetActive(true);
     }

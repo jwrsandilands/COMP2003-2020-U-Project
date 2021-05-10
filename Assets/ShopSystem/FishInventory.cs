5ecfa60db40e02562147d4c580e476abfdb64620
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2021-05-10 19:37:10 +0100

    Updated FishInventory.cs
    Added comments to the code.

--- Assets/ShopSystem/FishInventory.cs
+++ Assets/ShopSystem/FishInventory.cs
@@ -4,22 +4,24 @@ using UnityEngine;
 
 public class FishInventory : MonoBehaviour
 {
-    public FishItem[] fishes;
-    private int index;
+    public FishItem[] fishes; //a collection of fishes currently caught
+    public int index; //gets the current element of the array
 
-    public bool isFull;
+    public bool isFull; //checks if the inventory is full
 
-    public void AddToArray(FishItem fishElement)
+
+    
+    public void AddToArray(FishItem fishElement) //fishelement is the object going to be added to the array
     {
-        if(index != fishes.Length)
+        if(index != fishes.Length) //if index is not equal to the length of the array
         {
-            fishes[index] = fishElement;
-            index++;
+            fishes[index] = fishElement; //add the object to the array
+            index++; //add the index
         }
         else
         {
-            isFull = true;
-            Debug.Log("You cannot hold anymore fish");
+            isFull = true; //set isfull to true
+            Debug.Log("You cannot hold anymore fish"); //displays this message in the console 
         }
     }
 }

3e549e00c2c784370d9e9a262ef1e33d3b49f9d3
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2021-05-12 14:59:51 +0100

    Updated ShopEntrance.cs
    Commented out shopentrancebutton and addfishbutton as they are no longer needed.. They were used for testing

--- Assets/ShopSystem/ShopEntrance.cs
+++ Assets/ShopSystem/ShopEntrance.cs
@@ -5,23 +5,27 @@ using UnityEngine;
 public class ShopEntrance : MonoBehaviour
 {
     public GameObject sellingMenu; //this is for the selling menu 
-    public GameObject shopEntranceButton; //a button for opening the shop
-    public GameObject addFishButton; //test button for adding fish (not needed)
+   // public GameObject shopEntranceButton; //a button for opening the shop
+    //public GameObject addFishButton; //test button for adding fish (not needed)
     public GameObject buyMenu; //this for the buying menu 
+
+    public GameObject fishAndCurrencyUI;
 
 
     public void EnterStore()
     {
         sellingMenu.SetActive(true); //enables visibility of shop UI
-        shopEntranceButton.SetActive(false); //disables visibility of shop entrance button
-        addFishButton.SetActive(false); //disables visibility of the add fish button
+        fishAndCurrencyUI.SetActive(true);
+        //shopEntranceButton.SetActive(false); //disables visibility of shop entrance button
+        //addFishButton.SetActive(false); //disables visibility of the add fish button
     }
 
     public void ExitStore()
     {
         sellingMenu.SetActive(false); //disables visibility of the selling menu 
         buyMenu.SetActive(false); //disables visibility of buying menu 
-        shopEntranceButton.SetActive(true); //enables visibility of shop entrance button
-        addFishButton.SetActive(true); //enables visibility of the add fish button
+        fishAndCurrencyUI.SetActive(false);
+        //shopEntranceButton.SetActive(true); //enables visibility of shop entrance button
+        //addFishButton.SetActive(true); //enables visibility of the add fish button
     }
 }
